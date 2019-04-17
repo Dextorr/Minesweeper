d7141fdadf78451e04c6af8e0b1e03cbf342f92f
Dexter De Leon <dexdeleon@gmail.com>
2019-04-17 14:30:54 +0100

    Refactored JS

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+const width = 8,
+  numOfMines = 10,
+  cells = [],
+  mines = [],
+  flags = [],
+  surrounding = [width -1, width, width+1, 1, -width+1, -width, -width -1, -1],
+  adjacent = [1, -1, width, -width]
+let gameBoard
 
-
-  const width = 8
-  const gameBoard = document.getElementById('gameBoard')
-  const cells = []
-  const numOfMines = 10
-  const mines = []
-  const flags = []
-  const surrounding = [width -1, width, width+1, 1, -width+1, -width, -width -1, -1]
-  const adjacent = [1, -1, width, -width]
-
+function createBoard(){
   for(let i=0;i<width**2;i++){
     const cell = document.createElement('div')
     cell.appendChild(document.createElement('p'))
@@ -19,7 +17,9 @@ document.addEventListener('DOMContentLoaded', () => {
     cells.push(cell)
     gameBoard.appendChild(cell)
   }
+}
 
+function placeMines(){
   for(let i=0;i<numOfMines;i++){
     let random = Math.floor(Math.random()* width**2)
     let mine = cells[random]
@@ -30,104 +30,106 @@ document.addEventListener('DOMContentLoaded', () => {
     mine.classList.add('mine')
     mines.push(mine)
   }
+}
 
-  function validCells(arr, index){
-    return arr.filter(cell => {
-      if (index%width === 0) {
-        const invalid = [width-1, -1, -width -1]
-        return !invalid.includes(cell)
-      } else if (index%width === width-1) {
-        const invalid = [width+1, 1, -width +1]
-        return !invalid.includes(cell)
-      } else return true
-    })
-  }
+function validCells(arr, index){
+  return arr.filter(cell => {
+    if (index%width === 0) {
+      const invalid = [width-1, -1, -width -1]
+      return !invalid.includes(cell)
+    } else if (index%width === width-1) {
+      const invalid = [width+1, 1, -width +1]
+      return !invalid.includes(cell)
+    } else return true
+  })
+}
 
-  function countSurrounding(index, className){
-    const toCheck = validCells(surrounding, index)
-    return toCheck.reduce((count, cell) => {
-      if(cells[index + cell] &&
+function countSurrounding(index, className){
+  const toCheck = validCells(surrounding, index)
+  return toCheck.reduce((count, cell) => {
+    if(cells[index + cell] &&
       cells[index+cell].classList.contains(className)){
-        return count + 1
-      } return count
-    }, 0)
-  }
+      return count + 1
+    } return count
+  }, 0)
+}
 
-  function isBlank(cell){
-    const gameClasses = ['num', 'mine']
-    return !gameClasses.some(gameClass => cell.classList.contains(gameClass))
-  }
+function isBlank(cell){
+  const gameClasses = ['num', 'mine']
+  return !gameClasses.some(gameClass => cell.classList.contains(gameClass))
+}
 
-  function clearBlanks(cell, index){
-    remove(cell, 'hidden')
-    if(isBlank(cell)){
-      clearNums(index)
-      const next = validCells(adjacent, index)
-      if (isBlank(cell)){
-        next.forEach(el => {
-          if(cells[index+el] &&
-            isBlank(cells[index+el]) &&
-            cells[index + el].classList.contains('hidden')
-          ){
-            clearBlanks(cells[index + el], index+el)
-          }
-        })
-      }
+function clearBlanks(cell, index){
+  remove(cell, 'hidden')
+  if(isBlank(cell)){
+    clearNums(index)
+    const next = validCells(adjacent, index)
+    if (isBlank(cell)){
+      next.forEach(el => {
+        if(cells[index+el] &&
+          isBlank(cells[index+el]) &&
+          cells[index + el].classList.contains('hidden')
+        ){
+          clearBlanks(cells[index + el], index+el)
+        }
+      })
     }
   }
+}
 
-  function clearNums(index){
-    const toClear = validCells(surrounding, index)
-    toClear.forEach(el => {
-      if(cells[index+el] &&
-        cells[index+el].classList.contains('num')
-      ) remove(cells[index+el], 'hidden')
-    })
-  }
+function clearNums(index){
+  const toClear = validCells(surrounding, index)
+  toClear.forEach(el => {
+    if(cells[index+el] &&
+      cells[index+el].classList.contains('num')
+    ) remove(cells[index+el], 'hidden')
+  })
+}
 
-  function explode(cell){
-    let currentStep = 0
+function remove(cell, className){
+  cell.classList.remove(className)
+}
+
+function explode(cell){
+  let currentStep = 0
+  const explosionTimer = setInterval(() => {
+    cell.setAttribute('data-step', currentStep)
+    currentStep = currentStep === 15 ? 0 : currentStep += 1
+    if(currentStep === 0) {
+      clearInterval(explosionTimer)
+    }
+  }, 50)
+}
+
+function checkForMine(cell){
+  if(cell.classList.contains('mine')){
+    alert('YOU LOSE!')
+    explode(cell)
+    let i = 0
     const explosionTimer = setInterval(() => {
-      cell.setAttribute('data-step', currentStep)
-      currentStep = currentStep === 15 ? 0 : currentStep += 1
-      if(currentStep === 0) {
-        clearInterval(explosionTimer)
-      }
-    }, 50)
+      if(i<mines.length){
+        remove(mines[i], 'hidden')
+        explode(mines[i])
+      } else clearInterval(explosionTimer)
+      i++
+    }, 100)
   }
+}
 
-  function remove(cell, className){
-    cell.classList.remove(className)
+function flagHandler(e, cell){
+  e.preventDefault()
+  if (flags.includes(cell)){
+    flags.splice(flags.indexOf(cell), 1)
+  } else flags.push(cell)
+  cell.classList.toggle('flag')
+  if (flags.every(flag => flag.classList.contains('mine')) &&
+  flags.length === numOfMines
+  ){
+    alert('YOU WIN!')
   }
+}
 
-  function checkForMine(cell){
-    if(cell.classList.contains('mine')){
-      alert('YOU LOSE!')
-      explode(cell)
-      let i = 0
-      const explosionTimer = setInterval(() => {
-        if(i<mines.length){
-          remove(mines[i], 'hidden')
-          explode(mines[i])
-        } else clearInterval(explosionTimer)
-        i++
-      }, 100)
-    }
-  }
-
-  function flagHandler(e, cell){
-    e.preventDefault()
-    if (flags.includes(cell)){
-      flags.splice(flags.indexOf(cell), 1)
-    } else flags.push(cell)
-    cell.classList.toggle('flag')
-    if (flags.every(flag => flag.classList.contains('mine')) &&
-      flags.length === numOfMines
-    ){
-      alert('YOU WIN!')
-    }
-  }
-
+function addEvents(){
   cells.forEach((cell, index) => {
 
     const count = countSurrounding(index, 'mine')
@@ -149,4 +151,13 @@ document.addEventListener('DOMContentLoaded', () => {
       flagHandler(e, cell)
     })
   })
-})
+}
+
+function init(){
+  gameBoard = document.getElementById('gameBoard')
+  createBoard()
+  placeMines()
+  addEvents()
+}
+
+document.addEventListener('DOMContentLoaded', init)
